2ee593816048ce329621b30ec92c938d9fdc5cf5
Roman <raman.hreshchyk@softswiss.com>
2022-03-31 22:09:14 +0300

    new object adding

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,12 @@
 "use strict";
 let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 alert(numberOfFilms);
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
