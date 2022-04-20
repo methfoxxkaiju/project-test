e899742535590e489e855b43057390a98e4a8a68
Roman <raman.hreshchyk@softswiss.com>
2022-04-20 16:33:27 +0300

    new files for practice part 4

--- JS/conditions.js
+++ JS/conditions.js
@@ -1,1 +1,24 @@
-"use struct";
+"use strict";
+function copy(mainObject) {
+    let objCopy = {};
+
+    for (let key in mainObject) {
+        objCopy[key] = mainObject[key];
+    }
+
+    return objCopy;
+}
+
+const numbers = {
+    a:2,
+    b:5,
+    c: {
+        x:2,
+        y:3
+    }
+};
+
+const newNumbers = copy(numbers);
+newNumbers.a = 10;
+console.log(newNumbers);
+console.log(numbers);
